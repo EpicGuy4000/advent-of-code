9f4f02182129aaad84fecde60c6f5549da120c22
m.enjakovic <m.enjakovic@vegait.rs>
2022-08-18 15:01:08 +0200

    Day 6 - Part 2

--- src/day6/day6.test.ts
+++ src/day6/day6.test.ts
@@ -1,20 +1,26 @@
 import {example, full} from "./day6.data";
 import {LanternfishSimulator} from "./day6";
 
-test('after 1 day for example data is 2, 3, 2, 0, 1', () => {
+test('after 0 days for example data expect 5 fish', () => {
+    const simulator = new LanternfishSimulator(example);
+
+    expect(simulator.getCurrentFishCount()).toBe(5);
+})
+
+test('after 1 day for example data expect 5 fish', () => {
     const simulator = new LanternfishSimulator(example);
 
     simulator.passTime(1);
 
-    expect(simulator.getCurrentFish().map(f => f.cycleTimer)).toEqual([2, 3, 2, 0, 1]);
+    expect(simulator.getCurrentFishCount()).toBe(5);
 })
 
-test('after 2 days for example data is 1, 2, 1, 6, 0, 8', () => {
+test('after 2 days for example data expect 6 fish', () => {
     const simulator = new LanternfishSimulator(example);
 
     simulator.passTime(2);
 
-    expect(simulator.getCurrentFish().map(f => f.cycleTimer)).toEqual([1, 2, 1, 6, 0, 8]);
+    expect(simulator.getCurrentFishCount()).toBe(6);
 })
 
 test('after 18 days for example data we have 26 fish', () => {
@@ -22,7 +28,7 @@ test('after 18 days for example data we have 26 fish', () => {
 
     simulator.passTime(18);
 
-    expect(simulator.getCurrentFish().length).toBe(26);
+    expect(simulator.getCurrentFishCount()).toBe(26);
 })
 
 test('after 80 days for example data we have 5934 fish', () => {
@@ -30,7 +36,7 @@ test('after 80 days for example data we have 5934 fish', () => {
 
     simulator.passTime(80);
 
-    expect(simulator.getCurrentFish().length).toBe(5934);
+    expect(simulator.getCurrentFishCount()).toBe(5934);
 })
 
 test('after 80 days for test data we print answer', () => {
@@ -38,7 +44,7 @@ test('after 80 days for test data we print answer', () => {
 
     simulator.passTime(80);
 
-    console.log("After 80 days we will have this many fish:", simulator.getCurrentFish().length);
+    console.log("After 80 days we will have this many fish:", simulator.getCurrentFishCount());
 })
 
 test('after 256 days for example data we have 26984457539 fish', () => {
@@ -46,7 +52,7 @@ test('after 256 days for example data we have 26984457539 fish', () => {
 
     simulator.passTime(256);
 
-    expect(simulator.getCurrentFish().length).toBe(26984457539);
+    expect(simulator.getCurrentFishCount()).toBe(26984457539);
 })
 
 test('after 256 days for test data we print answer', () => {
@@ -54,5 +60,5 @@ test('after 256 days for test data we print answer', () => {
 
     simulator.passTime(256);
 
-    console.log("After 256 days we will have this many fish:", simulator.getCurrentFish().length);
+    console.log("After 256 days we will have this many fish:", simulator.getCurrentFishCount());
 })
--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,47 +1,21 @@
-class Lanternfish {
-    constructor(cycleTimer: number, simulator: LanternfishSimulator) {
-        this.cycleTimer = cycleTimer;
-        this.simulator = simulator;
-    }
-
-    cycleTimer:number;
-    private readonly simulator:LanternfishSimulator;
-
-    tick(numberOfDays:number) {
-        if (this.cycleTimer === 0) {
-            this.cycleTimer = 7;
-        }
-        this.cycleTimer -= numberOfDays;
-        if (this.cycleTimer <= 0) {
-            this.simulator.addNewFish(new Lanternfish(this.cycleTimer + 9, this.simulator));
-            if (this.cycleTimer < 0) {
-                this.cycleTimer += 7;
-            }
-        }
-    }
-}
-
 export class LanternfishSimulator {
-    private readonly fish:Lanternfish[];
+    private readonly fish:number[];
 
     constructor(startingFish: number[]) {
-        this.fish = startingFish.map(x => new Lanternfish(x, this));
+        this.fish = new Array(9).fill(0);
+
+        startingFish.forEach(x => this.fish[x]++);
     }
 
     passTime(days:number) {
-        let daysPassed = days % 7;
-        [...this.fish].forEach(f => f.tick(daysPassed));
-        while (daysPassed < days) {
-            [...this.fish].forEach(f => f.tick(7));
-            daysPassed += 7;
+        for (let i = 0; i < days; i++) {
+            const fishWithZero = this.fish.shift();
+            this.fish[8] = fishWithZero;
+            this.fish[6] += fishWithZero;
         }
     }
 
-    addNewFish(newFish: Lanternfish) {
-        this.fish.push(newFish);
-    }
-
-    getCurrentFish() {
-        return this.fish.filter(f => f.cycleTimer < 9);
+    getCurrentFishCount() {
+        return this.fish.reduce((acc, x) => acc + x);
     }
 }
