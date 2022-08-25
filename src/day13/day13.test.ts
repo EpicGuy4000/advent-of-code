1d0728f574a1ada133fa7e0057e6395491616ae1
m.enjakovic <m.enjakovic@vegait.rs>
2022-08-25 13:26:07 +0200

    Day 13

---
+++ src/day13/day13.test.ts
@@ -0,0 +1,32 @@
+import {example, full} from "./day13.data";
+import {Folder} from "./day13";
+
+test('for example finds 18 dots and 2 fold instructions', () => {
+    const folder = new Folder(example);
+    expect(folder.dots.length).toBe(18);
+    expect(folder.folds.length).toBe(2);
+})
+
+test('parses fold instructions correctly', () => {
+    const folder = new Folder(example);
+    expect(folder.folds).toEqual([ { along: 'y', coordinate: 7 }, { along: 'x', coordinate: 5 } ]);
+})
+
+test('for example after 1 fold 17 dots are visible', () => {
+    const folder = new Folder(example);
+    folder.foldOnce();
+
+    expect(folder.dots.length).toBe(17);
+})
+
+test('for full input after 1 fold print how many dots are visible', () => {
+    const folder = new Folder(full);
+    folder.foldOnce();
+
+    console.log('After 1 fold', folder.dots.length, 'dots are visible');
+})
+
+test('for full input after all folds prints output', () => {
+    const folder = new Folder(full);
+    folder.fold();
+})
---
+++ src/day13/day13.ts
@@ -0,0 +1,91 @@
+class Coordinates {
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    x:number;
+    y:number;
+}
+
+interface FoldInstruction {
+    along:string,
+    coordinate:number
+}
+
+export class Folder {
+    dots:Coordinates[] = [];
+    folds:FoldInstruction[] = [];
+    private instructionCounter = 0;
+    private canvasSizeX = 0;
+    private canvasSizeY = 0;
+
+    constructor(instructions: string[]) {
+        let i = 0;
+        while (instructions[i] != '') {
+            const [x, y] = instructions[i].split(',').map(x => +x);
+            this.dots.push(new Coordinates(x, y))
+            if (x > this.canvasSizeX)
+                this.canvasSizeX = x;
+            if (y > this.canvasSizeY)
+                this.canvasSizeY = y;
+            i++;
+        }
+        i++;
+        for (;i < instructions.length; i++) {
+            const [along, coordinate] = instructions[i].slice(11).split('=');
+            this.folds.push({ along, coordinate:+coordinate});
+        }
+    }
+
+    foldOnce() {
+        const instruction = this.folds[this.instructionCounter];
+        this.instructionCounter++;
+
+        if (instruction.along === 'x') {
+            this.foldAlongX(instruction.coordinate);
+        }
+        else {
+            this.foldAlongY(instruction.coordinate);
+        }
+    }
+
+    fold() {
+        console.log('starting with');
+        this.print();
+        while (this.instructionCounter < this.folds.length) {
+            this.foldOnce();
+        }
+    }
+
+    private foldAlongX(coordinate: number) {
+        this.dots.filter(d => d.x > coordinate)
+            .forEach(d => d.x = 2 * coordinate - d.x);
+        this.dots = this.dots.filter((d, i, a) => a.find(d1 => d1.x === d.x && d1.y == d.y) === d);
+        this.canvasSizeX = Math.floor(this.canvasSizeX / 2) - 1;
+        console.log('after x fold along', coordinate);
+        this.print();
+    }
+
+    private foldAlongY(coordinate: number) {
+        this.dots.filter(d => d.y > coordinate)
+            .forEach(d => d.y = 2 * coordinate - d.y);
+        this.canvasSizeY = Math.floor(this.canvasSizeY / 2) - 1;
+        this.dots = this.dots.filter((d, i, a) => a.find(d1 => d1.x === d.x && d1.y == d.y) === d);
+        console.log('after y fold along', coordinate);
+        this.print();
+    }
+
+    print() {
+        const allRows = [];
+        for (let i = 0; i <= this.canvasSizeY; i++) {
+            const row = [];
+            for (let j = 0; j <= this.canvasSizeX; j++) {
+                row.push(!this.dots.find(d => d.x === j && d.y === i) ? '.' : '#');
+            }
+            allRows.push(row.join(''));
+        }
+
+        console.log(allRows.join('\n'));
+    }
+}
